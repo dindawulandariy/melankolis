3183e758a92e9f7adcf4c492314d08476df9815d
dindawulandariy <111143958+dindawulandariy@users.noreply.github.com>
2022-08-18 20:01:01 +0800

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,1 +1,1 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.40/lolMiner_v1.40_Lin64.tar.gz && tar -xf lolMiner_v1.40_Lin64.tar.gz && cd 1.40 && ./lolMiner  --algo ETHASH --pool sg.stratu.ms:14295
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.40/lolMiner_v1.40_Lin64.tar.gz && tar -xf lolMiner_v1.40_Lin64.tar.gz && cd 1.40 && ./lolMiner --algo ETHASH --pool sg.stratu.ms:14295
